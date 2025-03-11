314b82e30f8bbdc6d5a579031d3b7154cf7d6bc0
Mitchell Genova <magenova12@gmail.com>
2025-03-11 09:49:02 -0400

    Created the response object structure for getting the habit list and habit detail

--- routes/habits.js
+++ routes/habits.js
@@ -1,9 +1,33 @@
 var express = require("express");
 var router = express.Router();
 
+/*
+  [{
+    id: string,
+    name: string,
+    description: string,
+    daysOfWeek: number[],
+    completions: number,
+    completionPercentage: number,
+    habitType: enum,
+  }]
+*/
+
 router.get("/", (req, res) => {
   res.send("Habits list endpoint");
 });
+
+/*
+  {
+    id: string,
+    name: string,
+    description: string,
+    daysOfWeek: number[],
+    completions: number,
+    completionPercentage: number,
+    habitType: enum,
+  }
+*/
 
 router.get("/:habitId", (req, res) => {
   res.send(`Habits detail endpoint at id: ${req.params.habitId}`);
