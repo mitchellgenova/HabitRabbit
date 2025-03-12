c7ceb5b0d49d1843a50aa3b4d550182e24f1d9e4
Mitchell Genova <magenova12@gmail.com>
2025-03-12 10:27:56 -0400

    Setup the users router to let me create and view all users. Using Postman to test out user creation as well as work towards habit creation. Going to add in middleware soon for authentication and then I can pull userIds from there, but for now I need to manually pass the userId in for habit creation

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,22 @@
 var express = require("express");
 var router = express.Router();
+var prisma = require("../const/prisma");
 
 /* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
+router.get("/", async (req, res) => {
+  const allUsers = await prisma.user.findMany();
+  res.send({ allUsers });
+});
+
+router.post("/", async (req, res) => {
+  const { email } = req.body;
+  await prisma.user.create({
+    data: {
+      email,
+    },
+  });
+
+  res.send({});
 });
 
 module.exports = router;
