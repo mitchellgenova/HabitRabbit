23385898aa02232429a108f470b0f8b28aef3d90
Mitchell Genova <magenova12@gmail.com>
2025-03-12 11:22:25 -0400

    Added in bcrypt to encrypt passwords. Setup a proper signup and login route

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ var express = require("express");
 var router = express.Router();
 var jwt = require("jsonwebtoken");
 var prisma = require("../const/prisma");
+const bcrypt = require("bcrypt");
 
 const secretKey = process.env.JWT_SECRET_KEY;
 
@@ -10,21 +11,24 @@ router.get("/", async (req, res) => {
   res.send({ allUsers });
 });
 
-router.post("/", async (req, res) => {
+router.post("/signup", async (req, res) => {
   const { email, password, name } = req.body;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
   await prisma.user.create({
     data: {
       email,
-      password,
+      password: hashedPassword,
       name,
     },
   });
 
-  res.send({});
+  res.status(201).json({ message: "User registered successfully " });
 });
 
 router.post("/login", async (req, res) => {
-  const { email } = req.body;
+  const { email, password } = req.body;
 
   const user = await prisma.user.findUnique({
     where: {
@@ -36,8 +40,7 @@ router.post("/login", async (req, res) => {
     return res.status(401).json({ error: "User not found" });
   }
 
-  // Write this later
-  const passwordMatch = true;
+  const passwordMatch = await bcrypt.compare(password, user.password);
 
   if (!passwordMatch) {
     return res.status(401).json({ error: "Authentication Failed" });
