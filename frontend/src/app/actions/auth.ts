0c76da6a7d125e631ce49a86da05ae2316dffa3d
Mitchell Genova <magenova12@gmail.com>
2025-03-13 15:09:08 -0400

    Removed the requirement for names to be unique in the prisma schema. Setup a sign in page. Added a redirect from the sign up page to the sign in page. Started setting up the cookies and auth on the frontend

--- frontend/src/app/actions/auth.ts
+++ frontend/src/app/actions/auth.ts
@@ -1,6 +1,12 @@
-import { SignupFormSchema, FormState } from "@/app/lib/definitions";
+import {
+  SigninFormSchema,
+  SigninFormState,
+  SignupFormSchema,
+  SignupFormState,
+} from "@/app/lib/definitions";
+import { redirect } from "next/navigation";
 
-export async function signup(state: FormState, formData: FormData) {
+export async function signup(state: SignupFormState, formData: FormData) {
   // Validate form fields
   const validatedFields = SignupFormSchema.safeParse({
     name: formData.get("name"),
@@ -21,7 +27,7 @@ export async function signup(state: FormState, formData: FormData) {
   const data = await fetch("http://localhost:3010/users/signup", {
     method: "POST",
     headers: {
-      "Content-Type": "application/json", // Specify that you're sending JSON
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({
       name,
@@ -29,4 +35,37 @@ export async function signup(state: FormState, formData: FormData) {
       password,
     }),
   });
+
+  redirect("/signin");
+}
+
+export async function signin(state: SigninFormState, formData: FormData) {
+  // Validate form fields
+  const validatedFields = SigninFormSchema.safeParse({
+    email: formData.get("email"),
+    password: formData.get("password"),
+  });
+
+  // If any form fields are invalid, return early
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+    };
+  }
+
+  // 2. Prepare data for insertion into database
+  const { email, password } = validatedFields.data;
+
+  const data = await fetch("http://localhost:3010/users/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      email,
+      password,
+    }),
+  });
+
+  console.log(data);
 }
