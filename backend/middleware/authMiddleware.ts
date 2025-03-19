b5e7c04d1837364eebf33a2b753a8e372aaeedad
Mitchell Genova <magenova12@gmail.com>
2025-03-19 10:05:16 -0400

    Added in typing for the authMiddleware file

--- backend/middleware/authMiddleware.ts
+++ backend/middleware/authMiddleware.ts
@@ -1,8 +1,13 @@
+import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 
-const secretKey = process.env.JWT_SECRET_KEY;
+const secretKey = process.env.JWT_SECRET_KEY as string;
 
-const verifyToken = (req, res, next) => {
+interface DecodedToken {
+  userId: string;
+}
+
+const verifyToken = (req: Request, res: Response, next: NextFunction) => {
   const token = req.cookies.token;
   console.log(req.cookies);
 
@@ -11,8 +16,9 @@ const verifyToken = (req, res, next) => {
   }
 
   try {
-    const decoded = jwt.verify(token, secretKey);
+    const decoded = jwt.verify(token, secretKey) as DecodedToken;
     req.userId = decoded.userId;
+
     next();
   } catch (error) {
     res.status(401).json({ error: "Invalid token" });
