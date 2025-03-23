d44d54f7f7859f1d5e843eeba34dcaa3dd3e9e3c
Mitchell Genova <magenova12@gmail.com>
2025-03-23 08:33:25 -0400

    Created a font file where I am exporting inter as my primary font

---
+++ frontend/font.ts
@@ -0,0 +1,7 @@
+import { Inter } from "next/font/google";
+
+// If loading a variable font, you don't need to specify the font weight
+export const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+});
